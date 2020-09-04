91978da4768633ed73808efa9242c013a9f7e428
Denperidge <denperidge@gmail.com>
2020-09-04 12:57:08 +0200

    Added icon generation

--- extract.bat
+++ extract.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 cd %~dp0
 
@@ -7,14 +8,53 @@ for /f "delims== tokens=1,2" %%G in (.env) do set %%G=%%H
 
 for %%t in (%*) do (
     %MHW_TEXT_CONVERTER_BY_JODO% "%%t"
-    
 )
 
 
 for %%d in (%*) do (
     REM Output dir + "BC7_" + filename + ".dds"
-    echo "%%~dpdBC7_%%~nd.dds"
-    %NVIDIA_TEXTURE_TOOLS_EXPORTER% "%%~dpdBC7_%%~nd.dds" --format bc7 --output "%%~dpdBC7_%%~nd.png"
+    set oldfilename="%%~dpdBC7_%%~nd.dds"
+    set newfilename="%%~dpdBC7_%%~nd.png"
+    
+    echo "Converting tex to dds"
+    %NVIDIA_TEXTURE_TOOLS_EXPORTER% !oldfilename! --format bc7 --output !newfilename!
+    magick convert !newfilename! -trim !newfilename! 
+
+    set icon="y"
+
+    if !icon!=="y" (
+        REM Saving output to variable: https://stackoverflow.com/a/2340018/5522348
+        REM cmd console command: magick identify -format "%[w]" "BC7_cmn_creature00_ID.png
+        magick identify -format "%%[w]" !newfilename! > width.tmp
+        magick identify -format "%%[h]" !newfilename! > height.tmp
+
+
+        set /p width=<width.tmp
+        set /p height=<height.tmp
+
+        echo !width!
+        echo !height!
+        
+        explorer.exe !newfilename!
+
+        set /p hor="# of icons horizontally: "
+        set /p ver="# of icons vertically: "
+
+        set /a iconwidth=!width! / !hor! + 1
+        set /a iconheight=!height! / !ver! + 1
+
+        echo !iconheight!
+
+
+        for /L %%v in (0,!iconheight!,!height!) do (
+            echo "V: %%v"
+            for /L %%h in (0,!iconwidth!,!width!) do (
+                echo "!iconwidth!x!iconheight!+%%h+%%v"
+                magick convert -extract "!iconwidth!x!iconheight!+%%h+%%v" !newfilename! "!newfilename!%%h%%v.png"
+            )
+        )
+    )
 )
     
     
+REM magick convert -extract 1024x1024+0+0 BC7_cmn_creature00_ID.png test.png
